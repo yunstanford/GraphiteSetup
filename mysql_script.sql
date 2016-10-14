cd099a55efdbf18cf859b817f8d67aa157186659
Yun Xu <yunx@zillowgroup.com>
2016-10-14 10:43:54 -0700

    improve mysql_script.sql

--- mysql_script.sql
+++ mysql_script.sql
@@ -1,8 +1,8 @@
-CREATE USER 'my_graphite'@'localhost' IDENTIFIED BY 'hello_world';
+CREATE USER IF NOT EXISTS 'my_graphite'@'localhost' IDENTIFIED BY 'hello_world';
 SELECT 'Create user: my_graphite with password: hello_world.' AS ' ';
 GRANT ALL PRIVILEGES ON * . * TO 'my_graphite'@'localhost';
 SELECT 'Grant privileges to the user: my_graphite.' AS ' ';
-CREATE DATABASE graphite_backend;
+CREATE DATABASE IF NOT EXISTS graphite_backend;
 SELECT 'Create database graphite_backend' AS '';
 USE mysql;
 SELECT user, host FROM user WHERE user = "my_graphite";
