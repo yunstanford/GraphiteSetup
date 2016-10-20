5c9e2cb13aa1c8f0cec41728ea18c078017849c7
Mayur Mahajan <mayurm@zillowgroup.com>
2016-10-20 14:35:38 -0700

    MON-128004: run carbon rely ng and not the usual relay

--- setup_carbon_relay_ng.sh
+++ setup_carbon_relay_ng.sh
@@ -1,12 +1,18 @@
 SCRIPT_PATH=$(pwd)
-echo $SCRIPT_PATH
 GOPATH=$SCRIPT_PATH/go
 mkdir -p $GOPATH
 mkdir -p /tmp/carbon-relay-ng/spool
 export GOPATH=$GOPATH
 export PATH="$PATH:$GOPATH/bin"
+
+echo "[INFO] ===== Cloning carbon-relay-ng ===== "
 go get -d github.com/graphite-ng/carbon-relay-ng
+echo "[INFO] ===== Cloning go-bindata ===== "
 go get github.com/jteeuwen/go-bindata/...
+
+echo "[INFO] ===== Compiling carbon-relay-ng ===== "
 cd $GOPATH/src/github.com/graphite-ng/carbon-relay-ng
 make
+
+echo "[INFO] ===== Moving compiled carbon-relay-ng binary to bin folder ===== "
 mv $GOPATH/src/github.com/graphite-ng/carbon-relay-ng/carbon-relay-ng $SCRIPT_PATH/bin/
