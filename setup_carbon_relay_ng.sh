ff7c4ef4a1db69d9532a638e1e55ca6bacea7936
Mayur Mahajan <mayurm@zillowgroup.com>
2016-10-26 17:15:06 -0700

    MON-129961: get go working

--- setup_carbon_relay_ng.sh
+++ setup_carbon_relay_ng.sh
@@ -1,11 +1,12 @@
 echo "INSIDE SETUP CARBON RELAY NG SCRIPT"
 SCRIPT_PATH=$(pwd)
-#mkdir -p $GOROOT
-mkdir -p /tmp/carbon-relay-ng/spool
-#export GOROOT=$GOROOT
-#export PATH="$PATH:$GOROOT/bin"
 
-echo $GOROOT
+mkdir -p $GOPATH
+mkdir -p /tmp/carbon-relay-ng/spool
+#export GOPATH=$GOPATH
+#export PATH="$PATH:$GOPATH/bin"
+
+echo $GOPATH
 echo $PATH
 echo $SCRIPT_PATH
 
@@ -15,8 +16,8 @@ echo "[INFO] ===== Cloning go-bindata ===== "
 go get github.com/jteeuwen/go-bindata/...
 
 echo "[INFO] ===== Compiling carbon-relay-ng ===== "
-cd $GOROOT/src/github.com/graphite-ng/carbon-relay-ng
+cd $GOPATH/src/github.com/graphite-ng/carbon-relay-ng
 make
 
 echo "[INFO] ===== Moving compiled carbon-relay-ng binary to bin folder ===== "
-mv $GOROOT/src/github.com/graphite-ng/carbon-relay-ng/carbon-relay-ng $SCRIPT_PATH/bin/
+mv $GOPATH/src/github.com/graphite-ng/carbon-relay-ng/carbon-relay-ng $SCRIPT_PATH/bin/
